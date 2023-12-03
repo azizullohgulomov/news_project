28bf3536c4d8a408a75758aed04392f09ab2e2c0
Azizulloh Gulomov <azizullohgulomov03@gmail.com>
2023-12-03 09:04:01 +0500

    users, password, login, register

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 from .views import (news_list, news_detail, homePageView, ContactPageView, singlePageView, HomePageView,
-                    LocalNewsView, GlobalNewsView, TechnologyNewsView, SportNewsView)
+                    LocalNewsView, GlobalNewsView, TechnologyNewsView, SportNewsView, NewsUpdateView, NewsDeleteView,
+                    NewsCreateView, admin_page_view, SearchResultsList)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('news/', news_list, name='all_news_list'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
     path('news/<slug:news>/', news_detail, name='news_detail_page'),
+    path('news/<slug>/edit', NewsUpdateView.as_view(), name='news_update'),
+    path('news/<slug>/delete', NewsDeleteView.as_view(), name='news_delete'),
     path('contact-us/', ContactPageView.as_view(), name='contact_page'),
     path('single-page/', singlePageView, name='single_page'),
     path('local-news/', LocalNewsView.as_view(), name='local_news_page'),
     path('global-news/', GlobalNewsView.as_view(), name='global_news_page'),
     path('technology-news/', TechnologyNewsView.as_view(), name='technology_news_page'),
-    path('sport-news/', SportNewsView.as_view(), name='sport_news_page')
+    path('sport-news/', SportNewsView.as_view(), name='sport_news_page'),
+    path('adminpage/', admin_page_view, name='admin_page'),
+    path('searchresult/', SearchResultsList.as_view(), name='search_results'),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.db.models import Q
+from hitcount.views import HitCountMixin
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from .models import News, Category
-from .forms import ContactForm
-from django.views.generic import TemplateView, ListView
+from .forms import ContactForm, CommentForm
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required, user_passes_test
+from news_project.custom_permissions import OnlyLoggedSuperUser
+
 
 # Create your views here.
 def news_list(request):
@@ -13,13 +21,48 @@ def news_list(request):
     }
     return render(request, 'news/news_list.html', context)
 
+from hitcount.views import HitCountDetailView
+
 def news_detail(request, news):
     news = get_object_or_404(News, slug=news, status=News.Status.Published)
+    context = {}
+    #hitcount logic
+    hit_count = get_hitcount_model().objects.get_for_objects(news)
+    hits = hit_count.hits
+    hit_context = context['hitcount'] = {'pk': hit_count.pk}
+    hit_count_response = HitCountMixin.hit_count(request, hit_count)
+    if hit_count_response.hit_counted:
+        hits = hits + 1
+        hitcontext['hit_counted'] = hit_count_response.hit_counted
+        hitcontext['hit_message'] = hit_count_response.hit_message
+        hitcontext['total_hits'] = hits
+
+    comments = news.comments.filter(active=True)
+    comment_count = comments.count()
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.news = news
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+
+        else:
+            comment_form = CommentForm()
+
     context = {
-        'news': news
+        'news': news,
+        'comments': comments,
+        'comment_count': comment_count,
+        'new_comment': new_comment,
+        'comment_form': comment_form
     }
 
     return render(request, 'news/news_detail.html', context)
+
 
 def homePageView(request):
     categories = Category.objects.all()
@@ -118,3 +161,44 @@ class SportNewsView(ListView):
     def get_queryset(self):
         news = self.model.published.all().filter(category__name='Sport')
         return news
+
+
+class NewsUpdateView(OnlyLoggedSuperUser, UpdateView):
+    moder = News
+    fields = ('title', 'body', 'image', 'category', 'status')
+    template_name = 'crud/news_edit.html'
+
+
+class NewsDeleteView(OnlyLoggedSuperUser, DeleteView):
+    model = News
+    template_name = 'crud/news_delete.html'
+    success_url = reverse_lazy('home_page')
+
+
+class NewsCreateView(OnlyLoggedSuperUser, CreateView):
+    model = News
+    template_name = 'crud/news_create.html'
+    fields = ('title', 'slug', 'image', 'body', 'image', 'category', 'status')
+
+@login_required
+@user_passes_test(lambda u:u.is_superuser)
+def admin_page_view(request):
+    admin_users = User.objects.filter(is_superuser=True)
+
+    context = {
+        'admin_users': admin_users
+    }
+    return render(request, 'pages/admin_page.html', context)
+
+
+class SearchResultsList(ListView):
+    model = News
+    template_name = 'news/search_result.html'
+    context_object_name = 'barcha_yangilikar'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return News.objects.filter(
+            Q(title__icontains=query) | Q(body__icontains=query)
+
+        )
